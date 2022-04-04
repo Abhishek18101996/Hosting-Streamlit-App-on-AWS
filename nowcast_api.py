8fd8247c2fe45ae0f85a6ad5eeb3547b76e72788
Abhishek <abhishekjaiswal1918@gmail.com>
2022-04-04 17:46:59 -0400

    Add files via upload

--- nowcast_api.py
+++ nowcast_api.py
@@ -7,35 +7,105 @@ Created on Mon Mar  7 10:59:57 2022
 """
 
 import os
-from nowcast_helper import get_nowcast_data, run_model, save_gif, save_h5, writeDataToCloud
+from nowcast_helper import get_nowcast_data, run_model, writeDataToCloud, flushCache
 import dateutil.parser
-
-# Use the following for testing nowcast(lat=37.318363, lon=-84.224203, radius=100, time_utc='2019-06-02 18:33:00', catalog_path='C:\\Users\\krish\\Documents\\Northeastern University\\Spring22\\DAMG 7245\\Assignment-4\\CATALOG.csv', model_path='C:\\Users\\krish\\Documents\\Northeastern University\\Spring22\\DAMG 7245\\Assignment-4\\models', data_path='C:\\Users\\krish\\Documents\\Northeastern University\\Spring22\\DAMG 7245\\Assignment-4\\sevir',out_path='C:\\Users\\krish\\Documents\\Northeastern University\\Spring22\\DAMG 7245\\Assignment-4\\output', model_type='gal', closest_radius=True)
-def nowcast(lat, lon, radius, time_utc, model_type, catalog_path, model_path, data_path, out_path, closest_radius=False):
+import datetime
+import numpy as np
+# Use the following for testing nowcast(lat=35.909375, lon=-100.265042, radius=200, time_utc='2019-06-15 05:05:00', model_type='gan',closest_radius=True,threshold_time_minutes= 60, force_refresh=False)
+def nowcast(lat, lon, radius, time_utc, model_type, closest_radius=False, threshold_time_minutes= 60, force_refresh=False):
     Error = None
+    
     # Parse time
     try:
         user_time = dateutil.parser.parse(time_utc)
     except Exception:
         Error = 'Invalid date time format. Please provide a valid format (refer to https://dateutil.readthedocs.io/en/stable/parser.html)'
         return {'Error': Error}
+    
     # Data cannot be older than 2019 June 1st (As per paper)
     if user_time.month < 6:
         if user_time.year < 2019:
            Error = 'Request date is too old! Try dates after 2019, June 1st'
            return {'Error': Error}
     
-    
     try:
         # Filter to get data
-        data = get_nowcast_data(lat= lat, lon=lon, radius= radius, time_utc = time_utc, catalog_path = catalog_path, data_path = data_path, closest_radius=closest_radius)
+        exists, data, filename = get_nowcast_data(lat = lat, lon = lon, radius = radius, time_utc = time_utc, catalog_path = 'sevir-vil/CATALOG.csv', data_path = 'sevir-vil', closest_radius = closest_radius, threshold_time_minutes = threshold_time_minutes, force_refresh = force_refresh)
+        if exists:
+            return {'display':filename}
+        
         # Run model
-        output = run_model(data,model_path,scale=True,model_type=model_type)
-        # Output as h5/GIF
-        display_path = writeDataToCloud(data = output,file_path =os.path.join(out_path,f'latest_nowcast_display_{lat}_{lon}.gif'),file_type='gif', time_utc=time_utc)
-        # output_path = save_h5(output,os.path.join(out_path,f'nowcast_output_{lat}_{lon}.h5'))
+        output = run_model(data, 'sevir-vil/models/nowcast/', scale = True, model_type = model_type)
+        
+        # Timestamp when API is called and GIF is built
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        
+        # Output GIF
+        display_path = writeDataToCloud(data = output, file_path = os.path.join('sevir-vil/output',f'Predicted{filename}_{timestamp}.gif'), file_type='gif',time_utc=time_utc)
+
     except Exception as e:
         return {'Error': str(e)}
     
     # Return path for output
     return {'display':display_path}
+
+
+# Use the following for testing nowcast(lat=37.318363, lon=-84.224203, radius=100, time_utc='2019-06-02 18:33:00', model_type='gan',closest_radius=True)
+def nowcastBatch(params_list):
+    # Same code as nowcast, but in loop for all inputs
+    filename_final = []
+    data_final = []
+    data_index = []
+    display_path_final = []
+    
+    for param in params_list:
+        Error = None
+        
+        # Parse time
+        try:
+            print(param.time_utc)
+            user_time = dateutil.parser.parse(param.time_utc)
+        except Exception:
+            Error = 'Invalid date time format. Please provide a valid format (refer to https://dateutil.readthedocs.io/en/stable/parser.html)'
+            return {'Error': Error}
+        
+        # Data cannot be older than 2019 June 1st (As per paper)
+        if user_time.month < 6:
+            if user_time.year < 2019:
+               Error = 'Request date is too old! Try dates after 2019, June 1st'
+               return {'Error': Error}
+    
+        try:
+            # Filter to get data as BaseModel parameters
+            exists, data, filename = get_nowcast_data(lat = param.lat, lon = param.lon, radius = param.radius, time_utc = param.time_utc, catalog_path = 'sevir-vil/CATALOG.csv', data_path = 'sevir-vil', closest_radius = param.closest_radius, threshold_time_minutes = param.threshold_time_minutes, force_refresh = True)
+            if exists:
+                return {'Error': 'Fatal schedule error, checking in existing directory for fresh batch'}
+            if data_final == []:
+                data_final = data
+            else:
+               # Stack all input params as one and then run the model, split later into individual outputs 
+               data_final = np.vstack((data_final,data))
+            data_index.append(data.shape[0])
+            filename_final.append(filename)
+        except Exception as e:
+            return {'Error': str(e)}
+    
+    # Run model
+    output = run_model(data_final, 'sevir-vil/models/nowcast/', scale = True, model_type = param.model_type)
+    prev_index = 0
+    
+    for index,filename in zip(data_index,filename_final):
+        # Timestamp when API is called and GIF is built
+        timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        
+        # Flush already existing file in Cache
+        flushCache(folder = 'sevir-vil/cache', file = f'Predicted{filename}')
+        
+        # Split output GIF for all input sets
+        display_path = writeDataToCloud(data = output[prev_index:prev_index+index], file_path = os.path.join('sevir-vil/cache',f'Predicted{filename}_{timestamp}.gif'), file_type='gif',time_utc=param.time_utc)
+        prev_index+=index
+        display_path_final.append(display_path)
+        
+    
+    # Return path for output
+    return {'display':display_path_final}
--- streamlit-app.py
+++ streamlit-app.py
@@ -7,7 +7,11 @@ Created on Wed Mar 23 14:31:40 2022
 import streamlit as st
 import requests
 import base64
-
+import gcsfs
+from urllib.parse import unquote
+import pathlib
+import os
+abspath = pathlib.Path(__file__).parent.resolve()
 def main():
     st.title("API for Federal Avaiation Administration")
     html_temp = """
@@ -16,28 +20,76 @@ def main():
         </div>
         """
     st.markdown(html_temp, unsafe_allow_html=True)
-    lat = st.number_input("Latitude:", format="%.6f")
-    lon = st.number_input("Longitude:", format="%.6f")
-    radius = st.number_input("Radius:")
-    time_utc = st.text_input("Time UTC:")
-    model_type = st.text_input("Model Type:")
-    # closest_radius = st.text_input("Would you like to get the closest point, if location not found in chosen radius? (True or False)")
-    closest_radius = st.radio("Would you like to get the closest point, if location not found in chosen radius?", ("True", "False"))
+    username = st.text_input("User Name: ")
+    password = st.text_input("Password: ", type="password")
     
-    # Parameters as JSON
-    params_test = {"lat": lat, "lon": lon, "radius": radius, "time_utc": time_utc, "model_type": model_type, "closest_radius": closest_radius}
-    
-    if st.button("Predict"):
-        nowcast_test = requests.post("http://127.0.0.1:8000/nowcast/", json = params_test)      
-        sevir_output_test = nowcast_test.json()
-        if 'nowcast_error' in sevir_output_test.keys():
-            st.error({'nowcast_error': sevir_output_test['nowcast_error']})
-        else:
-            # st.success('OUTPUT: {}'.format(sevir_output_test))
-            st.success('Nowcasted GIF for the requested inputs: ')
-            gif_content = open(sevir_output_test['gif_path'], 'rb').read()
-            data_url = base64.b64encode(gif_content).decode("utf-8")
-            st.markdown(f'<p align="center"><img src="data:image/gif;base64,{data_url}" alt="Nowcasted GIF"></p>', unsafe_allow_html=True)
+    authjson = { "username": f"{username}", "password": f"{password}"}
+
+    s = st.session_state
+    if not s:
+        s.authenticated = False
+    col1, col2 = st.columns([0.2,1])
+    if col1.button("Login")  or s.authenticated:
+        s.authenticated = True
+        token = requests.post("https://nowcast-api-test-345519.ue.r.appspot.com/token", data = authjson)
+        if col2.button("Log Out"):
+            s.authenticated = False
+            token = None
+            jwttoken = None
+            st.markdown(f"Log Out Successful")
+            return None
+        
+        # Login successful        
+        if token.status_code == 200: 
+            # Headers
+            jwttoken = token.json()['access_token']
+            headers = {"Authorization": f"Bearer {jwttoken}"}
+            # Params by user
+            lat = st.number_input("Latitude:", format="%.6f")
+            lon = st.number_input("Longitude:", format="%.6f")
+            radius = st.number_input("Radius:")
+            time_utc = st.text_input("Time in UTC:")
+            model_type = st.text_input("Model Type:")
+            threshold_time_minutes = st.number_input("Threshold Time in Minutes:", format="%.2f")
+            closest_radius = st.radio("Would you like to get the closest point, if location not found in chosen radius?", (True, False))
+            forced_refresh = st.radio("Would you like to get a fresh generation of output?", (True, False))
             
+            # Parameters as JSON
+            params_test = {"lat": lat, "lon": lon, "radius": radius, "time_utc": time_utc, "model_type": model_type, "threshold_time_minutes": threshold_time_minutes, "closest_radius": bool(closest_radius), "forced_refresh": bool(forced_refresh)}
+            
+            # Perform nowcast
+            if st.button("Predict"):
+                nowcast_test = requests.post("https://nowcast-api-test-345519.ue.r.appspot.com/nowcast/predict", headers=headers, json = params_test)      
+                sevir_output_test = nowcast_test.json()
+                # Check for error, if any
+                if 'nowcast_error' in sevir_output_test.keys():
+                    st.error({'nowcast_error': sevir_output_test['nowcast_error']})
+                else:
+                    # Read output from bucket
+                    st.success('Nowcasted GIF for the requested inputs: ')
+                    # Path handling with regex
+                    decoded = unquote(sevir_output_test['gif_path'])
+                    path = ''
+                    append=False
+                    for a in decoded.split('/'):
+                        if append and a!='o':
+                            path+='/'+a.split('?')[0]
+                        if a=='sevir-vil':
+                            path+=a
+                            append=True
+                    # Connect to bucket        
+                    project_name = 'Assignment-4'
+                    credentials = os.path.join(abspath,"cred.json")
+                    FS = gcsfs.GCSFileSystem(project=project_name, token=credentials)
+                    with FS.open(path, 'rb') as data_file:                
+                        gif_content = data_file.read()
+                    data_url = base64.b64encode(gif_content).decode("utf-8")
+                    st.markdown(f'<p align="center"><img src="data:image/gif;base64,{data_url}" alt="Nowcasted GIF"></p>', unsafe_allow_html=True)
+       # Login failed
+        else:
+            st.markdown(f"Login Failure. {token.json()['detail']}")
+    return None
+        
+    
 if __name__ == '__main__':
     main()
